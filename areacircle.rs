033fa2b441dcbc2623ea1b782f91ebded5a00d9c
Aekkee <107569390+Aekkee@users.noreply.github.com>
2023-07-17 23:06:16 +0700

    Delete areacircle.rs

--- areacircle.rs
+++
@@ -1,7 +0,0 @@
-fn main() { 
-    let args: Vec<String> = std::env::args().collect();  
-    let x_arg = if args.len() < 2 { "" } else { &args[1] };  
-    let x: f32 = x_arg.parse().unwrap_or(0.0);  
-    let y = 3.1416 * x * x;
-    println!("Cirlcle area: {}",y); 
-}
