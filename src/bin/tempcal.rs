2f56da6969e691dec785e225c18639f788fb37f7
Aekkee <107569390+Aekkee@users.noreply.github.com>
2023-07-20 19:14:08 +0700

    Update tempcal.rs
    Edit to prevent the crash.

--- src/bin/tempcal.rs
+++ src/bin/tempcal.rs
@@ -2,10 +2,12 @@
 //Ex. cargo run --bin FtoC 10
 fn main() {
     let args: Vec<String> = std::env::args().collect();  
-    if &args[1] == " "
-    {
+    if args.get(1) == None{
         println!("re-enter");
-    } if &args[1] == "CtoF"{
+    } 
+    else if args.get(2) == None{
+        println!("re-enter");
+    } else if &args[1] == "CtoF"{
         let y: f32 = args[2].parse().unwrap_or(0.0);  
         println!("C to F: {}", 1.8 * y + 32.0);
     } else if &args[1] == "FtoC"{
