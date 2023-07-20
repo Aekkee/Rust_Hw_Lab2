7c19c518ee6a5778fa693bb9c32f0dc7c075ed8f
Aekkee <107569390+Aekkee@users.noreply.github.com>
2023-07-20 18:52:48 +0700

    Update cli.rs
    I edit cli.rs to test other scenarios of  list_players.

--- tests/cli.rs
+++ tests/cli.rs
@@ -15,7 +15,22 @@ fn temp() {
 }
 
 #[test]
-fn listplayer() { 
+fn listplayer1() { 
     let mut cmd = Command::cargo_bin("list_players").unwrap(); 
     cmd.assert().success().stdout("Player 1: N/A\nPlayer 2: N/A\n");
 }
+
+#[test]
+fn listplayer2() { 
+    let mut cmd = Command::cargo_bin("list_players").unwrap(); 
+    cmd.arg("Paul").arg("John");
+    cmd.assert().success().stdout("Player 1: Paul\nPlayer 2: John\n");
+}
+
+#[test]
+fn listplayer3() { 
+    let mut cmd = Command::cargo_bin("list_players").unwrap(); 
+    cmd.arg("Paul");
+    cmd.assert().success().stdout("Player 1: Paul\nPlayer 2: N/A\n");
+}
+
