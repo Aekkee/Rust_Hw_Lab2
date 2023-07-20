a254c6a71ba7fa94619cc4f83d8cee0c1d71bcfd
Aekkee <107569390+Aekkee@users.noreply.github.com>
2023-07-20 19:02:38 +0700

    Update cli.rs
    Edit all tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,15 +3,24 @@ use assert_cmd::Command;
 #[test]
 fn areacircle() { 
     let mut cmd = Command::cargo_bin("areacircle").unwrap(); 
-    cmd.assert().success().stdout("Cirlcle area: 0\n");
+    cmd.arg("10");
+    cmd.assert().success().stdout("Cirlcle area: 314.15997\n");
 }
 
 #[test]
-fn temp() { 
+fn temp1() { 
     let mut cmd = Command::cargo_bin("tempcal").unwrap(); 
     cmd.arg("CtoF");
     cmd.arg("10");
     cmd.assert().success().stdout("C to F: 50\n");
+}
+
+#[test]
+fn temp2() { 
+    let mut cmd = Command::cargo_bin("tempcal").unwrap(); 
+    cmd.arg("FtoC");
+    cmd.arg("10");
+    cmd.assert().success().stdout("F to C: -12.222223\n");
 }
 
 #[test]
@@ -33,4 +42,5 @@ fn listplayer3() {
     cmd.arg("Paul");
     cmd.assert().success().stdout("Player 1: Paul\nPlayer 2: N/A\n");
 }
+
 
